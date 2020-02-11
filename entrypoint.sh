086347cf1afd21add900a3911c5bad2162e163ab
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2020-02-11 22:36:32 +0000

    Git name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh -l
 
 latexmk
+git config --local user.email '"action@github.com"'
+git config --local user.name '"GitHub Action"'
 git add .
 git commit -m '"Build latex"'
 git push
